7e4ca7fb6d26bf822b94f0d8d5e5835b1ff3892b
Tomas <tpenroz2024@alu.uct.cl>
2026-04-19 18:52:13 -0400

    se cambio el precio minimo a 1

--- ventas-project/src/frontend/features/sales/components/SalesForm.tsx
+++ ventas-project/src/frontend/features/sales/components/SalesForm.tsx
@@ -82,7 +82,7 @@ const SalesForm: React.FC<SalesFormProps> = ({ onAddSale }) => {
               <label className="text-sm font-medium text-gray-700 mb-1">Precio</label>
               <input
               type="number"
-              min="0"
+              min="1"
               value={item.price}
               onChange={(e) => handleItemmChange(index, "price", Number(e.target.value))}
               className="w-28 px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-800"
